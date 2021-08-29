ed6b9b70f1e1cdf4f7eeae057f4f8d8e647b793f
singhrahul17 <mail2rahulcr7@gmail.com>
2021-08-29 21:32:55 +0530

    Added dagger dependency and kick started

--- app/src/main/java/com/example/example_dagger_car/car/Car.java
+++ app/src/main/java/com/example/example_dagger_car/car/Car.java
@@ -1,4 +1,21 @@
 package com.example.example_dagger_car.car;
 
+import android.util.Log;
+
+import javax.inject.Inject;
+
 public class Car {
+
+    private Engine mEngine;
+    private Wheels mWheels;
+
+    @Inject
+    public Car(Engine mEngine, Wheels mWheels) {
+        this.mEngine = mEngine;
+        this.mWheels = mWheels;
+    }
+
+    void driving() {
+        Log.d("Car", "driving... ");
+    }
 }
